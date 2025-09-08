f0d6ff3b9e2d21b433905bf72d8c798e541392c7
neo451 <412444506@qq.com>
2025-09-08 23:55:08 +0800

    feat: tp.file.path tp.file.folder

--- lua/obsidian/templater/file.lua
+++ lua/obsidian/templater/file.lua
@@ -1,10 +1,43 @@
-return setmetatable({}, {
-	__index = function(_, k)
-		local api = require("obsidian.api")
+local M = {}
+
+local api = require("obsidian.api")
+local Path = require("obsidian.path")
+
+---@param abosulte boolean
+---@return string
+M.folder = function(abosulte)
+	local parent = api.current_note(0).path:parent()
+	assert(parent, "can not find folder for current note")
+	if abosulte then
+		return tostring(parent)
+	else
+		if Path.new(Obsidian.workspace.root) == parent then
+			return ""
+		end
+		return assert(parent:vault_relative_path({ strict = true }))
+	end
+end
+
+---@param relative boolean
+---@return string
+M.path = function(relative)
+	local path = api.current_note(0).path
+	assert(path, "can not find path for current note")
+	if relative then
+		return assert(path:vault_relative_path({ strict = true }))
+	else
+		return tostring(path)
+	end
+end
+
+return setmetatable(M, {
+	__index = function(t, k)
 		if k == "title" then
 			return api.current_note(0).title
 		elseif k == "tags" then
 			return api.current_note(0).tags
+		else
+			return rawget(t, k)
 		end
 	end,
 })
